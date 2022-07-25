b379413383608a3369abbc7be72cec8cb79dbd44
NonSwag <mrminecraft00@gmail.com>
2022-07-25 19:40:51 +0200

    0.2.0

--- .jlauncher/desktop.jl
+++ .jlauncher/desktop.jl
@@ -1,5 +1,12 @@
 [
   {
+    "index": 74,
+    "name": "server",
+    "icon": "default.png",
+    "file": "/home/david/Desktop/Coding/paper/buildserver/1.16.5/server.jar",
+    "running-directory": "/home/david/Desktop/Coding/paper/buildserver/1.16.5"
+  },
+  {
     "index": 123,
     "name": "PassProtect",
     "icon": "default.png",
---
+++ .jlauncher/server.sh
@@ -0,0 +1,1 @@
+java -jar /home/david/Desktop/Coding/paper/buildserver/1.16.5/server.jar
